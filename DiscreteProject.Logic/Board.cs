6cbdae5b0cdbb23317cf6f5adcd66d08061c17fe
mafiuu <52745332+mafiuu@users.noreply.github.com>
2024-04-22 15:44:01 -0600

    changed

--- DiscreteProject.Logic/Board.cs
+++ DiscreteProject.Logic/Board.cs
@@ -1,18 +1,30 @@
-﻿using System.Globalization;
+﻿using System.Drawing;
+using System.Globalization;
 
 namespace DiscreteProject.Logic;
 
 public class Board
 {
     public int Size { get; set; }
-    public Row[] Rows { get; set; }
+    private Row[] rows;
+    public Row[] Rows { get => rows; }
+    public Action ElementOfHAdded { get; set; }
     public Board(int size)
     {
         Size = size;
-        Rows = new Row[size];
+        rows = new Row[size];
         for (int i = 0; i < size; i++)
         {
-            Rows[i] = new Row(size);
+            rows[i] = new Row(size);
+        }
+    }
+    public Board(Board old)
+    {
+        Size = old.Size;
+        rows = new Row[Size];
+        for (int i = 0; i < Size; i++)
+        {
+            rows[i] = old.rows[i];
         }
     }
     public void SetRowConfiguration(int rowNumber, string input)
@@ -23,58 +35,55 @@ public class Board
         }
         for (int i = 0; i < Size; i++)
         {
-            Rows[rowNumber].SetText(input);
+            rows[rowNumber].SetText(input);
         }
     }
     public void ReduceBoardTo2x2()
     {
-        var board = this;
         int n = Size;
-        for (int layer = 0; layer < n - 2; layer++)
+        while (n >= 2)
         {
-            for (int i = layer; i < n - 1; i++)
+            for (int i = 0; i < n - 1; i++)
             {
-                if (board.Rows[layer].Cells[i].Value == '0')
+                if (rows[i].Cells[0].Value == '0' && rows[i + 1].Cells[0].Value == '0' && rows[i + 2].Cells[0].Value != '0')
                 {
-                    AddElementOfH(layer, i);
+                    var newCells = AddElementOfH(rows[i].Cells[0], rows[i + 1].Cells[0], rows[i + 2].Cells[0]);
+                    rows[i].Cells[0] = newCells.a;
+                    rows[i + 1].Cells[0] = newCells.b;
+                    rows[i + 2].Cells[0] = newCells.c;
                 }
-                if (board.Rows[i].Cells[layer].Value == '0')
+                else if (rows[0].Cells[i].Value == '0' && rows[0].Cells[i + 1].Value == '0' && rows[0].Cells[i + 2].Value != '0')
                 {
-                    AddElementOfH(i, layer);
+                    var newCells = AddElementOfH(rows[0].Cells[i], rows[0].Cells[i + 1], rows[0].Cells[i + 2]);
+                    rows[0].Cells[i] = newCells.a;
+                    rows[0].Cells[i + 1] = newCells.b;
+                    rows[0].Cells[i + 2] = newCells.c;
                 }
             }
+            n--;
         }
-        for (int i = 0; i < n; i++)
-        {
-            if (board.Rows[n - 1].Cells[i].Value == '0')
-            {
-                AddElementOfH(n - 1, i);
-            }
-            if (board.Rows[i].Cells[n - 1].Value == '0')
-            {
-                AddElementOfH(i, n - 1);
-            }
-        }
-
         if (IsBoardSolvable())
         {
-            throw new Exception("The current board can be solved.");
+            throw new Exception("Board is solvable");
         }
         else
         {
-            throw new Exception("The current board can't be solved.");
+            throw new Exception("Board is not solvable");
         }
     }
-    private void AddElementOfH(int i, int j)
+    private (Cell a, Cell b, Cell c) AddElementOfH(Cell a, Cell b, Cell c)
     {
-        Rows[i].Cells[j] = Rows[i].Cells[j].Value == '0' ? new Cell('X') : new Cell('0');
+        a.InvertValue();
+        b.InvertValue();
+        c.InvertValue();
+        return (a, b, c);
     }
 
     public bool IsBoardSolvable()
     {
         int n = Size;
-        return Rows[n - 2].Cells[n - 2].Value == 'X' && Rows[n - 2].Cells[n - 1].Value == 'X' &&
-               Rows[n - 1].Cells[n - 2].Value == 'X' && Rows[n - 1].Cells[n - 1].Value == 'X';
+        return rows[n - 2].Cells[n - 2].Value == 'X' && rows[n - 2].Cells[n - 1].Value == 'X' &&
+               rows[n - 1].Cells[n - 2].Value == 'X' && rows[n - 1].Cells[n - 1].Value == 'X';
 
     }
     public void ModifyCell(Cell cell)
